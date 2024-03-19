82881104a4ba8873cef867b2aa30f94b466046ee
chenyuanjye <jackbedford0428@gmail.com>
2024-03-19 11:10:34 +0800

    chmod adding tool

--- experimental-tools/add_device_info.py
+++ experimental-tools/add_device_info.py
@@ -2,14 +2,14 @@
 
 import os
 
-user_input = input("請填入 device.txt")
+user_input = input("請填入 device.txt\n")
 print(f"echo {user_input} > device.txt")
 os.system(f"echo {user_input} > device.txt")
 
-user_input = input("請填入 password.txt")
+user_input = input("請填入 password.txt\n")
 print(f"echo {user_input} > password.txt")
 os.system(f"echo {user_input} > password.txt")
 
-user_input = input("請填入 savedir.txt")
+user_input = input("請填入 savedir.txt\n")
 print(f"echo {user_input} > savedir.txt")
 os.system(f"echo {user_input} > savedir.txt")
