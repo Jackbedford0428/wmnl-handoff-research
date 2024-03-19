a09c86afa7ed9ff2beb0c6d90d1526692dae0a3a
chenyuanjye <jackbedford0428@gmail.com>
2024-03-19 11:14:42 +0800

    update

--- experimental-tools/add_device_info.py
+++ experimental-tools/add_device_info.py
@@ -5,11 +5,14 @@ import os
 user_input = input("請填入 device.txt\n")
 print(f"echo {user_input} > device.txt")
 os.system(f"echo {user_input} > device.txt")
+print("-----------------------------------")
 
 user_input = input("請填入 password.txt\n")
 print(f"echo {user_input} > password.txt")
 os.system(f"echo {user_input} > password.txt")
+print("-----------------------------------")
 
 user_input = input("請填入 savedir.txt\n")
 print(f"echo {user_input} > savedir.txt")
 os.system(f"echo {user_input} > savedir.txt")
+print("-----------------------------------")
